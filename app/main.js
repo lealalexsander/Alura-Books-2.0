4cb6c1ccc958a428d6dcec09acad5496bf8bba39
Alexsander Leal <alexsanderunr@gmail.com>
2025-02-13 18:54:56 -0300

    implemtando em javascript uma função para monstrar os livros do link, na plataforma web

---
+++ app/main.js
@@ -0,0 +1,7 @@
+let livros = [];
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch(endpointDaAPI);
+    livros = await res.json();
+}
